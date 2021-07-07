40d0327aba64198169d69bdb0540686c6a2e69c3
EduardoNuezWehbe <nuezwehbe@gmail.com>
2021-07-07 21:02:09 +0100

    Add petition to openFoodFacts on serachEleement endpoint

--- routes/openFoodFacts.js
+++ routes/openFoodFacts.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 
 const router = express.Router()
 const openFoodFactsUrl = 'https://es.openfoodfacts.org/'
+const openFoodFactsCategoryUrl = 'https://es.openfoodfacts.org/categoria/'
 
 
 /* ////////////////////////////////////////////////////////////////
@@ -13,7 +14,7 @@ router.get('/allElements', async (req, res) => {
     if (Object.keys(req.body).length === 1) {
         const data = await axios.get(`${openFoodFactsUrl}${req.body.page}.json`)
         .then(res =>  res.data)
-        console.log(data.page)
+
         if (data.products.length !== 0) {
             res.status(200).json({
                 data: data,
@@ -41,13 +42,26 @@ router.get('/allElements', async (req, res) => {
 /* ////////////////////////////////////////////////////////////////
                 Get producst filtered by name
 //////////////////////////////////////////////////////////////// */
-router.get('/searchElement', (req, res) => {
-    console.log(req.body)
+router.get('/searchElement', async (req, res) => {
+
     if (Object.keys(req.body).length === 2) {
-        res.status(200).json({
-            success: true,
-            msg: '[SUCCESS] Elements retrieved successfully...!'
-        })
+        const data = await axios.get(`${openFoodFactsCategoryUrl}${req.body.product}/${req.body.page}.json`)
+        .then(res =>  res.data)
+
+        if (data.products.length !== 0) {
+            res.status(200).json({
+                data: data,
+                success: true,
+                msg: '[SUCCESS] Elements retrieved successfully...!'
+            })
+        } else {
+            res.status(204).json({
+                data: data,
+                success: true,
+                msg: '[SUCCESS] No content available...!'
+            })
+        }
+
     } else {
         res.status(400).json({
             success: false,
