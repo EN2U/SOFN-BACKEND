dcdff9571a33c03b3771d613bd310990b84f21a8
EduardoNuezWehbe <nuezwehbe@gmail.com>
2021-07-08 12:19:54 +0100

    Add new user products Schema

---
+++ models/UserProduct.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const UserProduct = new Schema ({
+    userName: {
+        type: String,
+        required: [true, 'Please include an username...'],
+        unique: true
+    },
+    product: {
+        type: String,
+        required: [true, 'Please include a product name...']
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    price: {
+        type: Number,
+        required: [true, 'Please include a price for the product per /Kg....']
+    },
+    amount: {
+        type: String,
+        required: [true, 'Please include the amount of the product you have available...']
+    },
+    brand: {
+        type: String,
+        required: false
+    }
+
+})
+
+module.exports = mongoose.model('User', UserProduct)
