bfc3bffc2764763aaba6bf5f778bb9c772248d77
EduardoNuezWehbe <nuezwehbe@gmail.com>
2021-07-08 11:24:44 +0100

    Add connection to mongodb atlas

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors');
+// const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose')
+const fs = require('fs');
 
 const app = express()
 
+
+const credentials = fs.readFileSync('../Certificados/X509-cert-3808143451361577486.pem');
+
+mongoose.connect('mongodb+srv://sreipeme.hvazi.mongodb.net/TFG?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    sslKey: credentials,
+    sslCert: credentials 
+}).then(db => console.log(`DB is connected`))
+  .catch(err => console.error(err))
 
 // Settings
 
