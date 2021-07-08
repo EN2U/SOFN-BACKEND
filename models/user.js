37dbadeac7c74ae997c90a00f1872c43ad692fb9
EduardoNuezWehbe <nuezwehbe@gmail.com>
2021-07-08 12:04:57 +0100

    Add new Schema for Users

--- models/user.js
+++ models/user.js
@@ -2,5 +2,37 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const User = new Schema ({
-    
+    userName: {
+        type: String,
+        required: [true, 'Please include an username...'],
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: [true, 'Please include your fullName...']
+    },
+    location: [{
+        street: {
+            type: String,
+            required: [true, 'Please include a location...']
+        },
+        number: {
+            type: Number,
+            required: false
+        }
+    }],
+    phone: {
+        type: String,
+        required: [true, 'Please include your phone...']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please include your email...']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please include your password...']
+    }
 })
+
+module.exports = mongoose.model('User', User)
